faebf3c6285060f33b54696bfc9ec28f64a965ac
Roger-GG06 <roger.gg@students.salle.url.edu>
2026-06-16 17:52:34 +0200

    He creado las funciones auxiliares de login para comprovar las validaciones al igual que un load users para abrir el archivo y un save users q se tendra q modificar para el register

--- session.h
+++ session.h
@@ -3,7 +3,13 @@
 
 #include "structs.h"
 
+void loadUsers(user *users, int *userCount);
+void saveUsers(user *users, int numUsers);
+void initHardcodedUsers(user *users, int *numUsers);
 
+int findUserByUsername(user *users, int numUsers, char username[MAX_STR]);
+int validatePassword(user u, char password[MAX_STR]);
+int validatePin(user u, int pin);
 
 UserType login(user users[], int numUsers);
 
